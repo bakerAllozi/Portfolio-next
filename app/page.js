268b2002d6168e7617676ca6d7bab5cefe494631
bakerAllozi <bakerdes654@gmail.com>
2025-01-10 00:56:23 +0300

    fix pug

--- app/page.js
+++ app/page.js
@@ -1,15 +1,10 @@
 import Hero from "./components/Hero";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./components/Navbar";
-import Project from "./components/Project";
-import Project2 from "./components/Project2";
-import TypingText from "./components/TypingText";
 import Gy from "./components/Gy";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import React from "react";
-import { PiArrowBendUpRightBold } from "react-icons/pi";
 
 export default function Home() {
   return (
