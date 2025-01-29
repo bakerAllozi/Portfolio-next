09d4a88f12a21721d50f17de7d9121b3d9c9020e
bakerAllozi <bakerdes654@gmail.com>
2025-01-29 08:55:23 +0300

    add fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals = { canvas: "commonjs canvas" };
+    return config;
+  },
+};
 
 export default nextConfig;
